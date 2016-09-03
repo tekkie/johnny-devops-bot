2fad4ba6369154b1e6a7b619398020863344f6b2
Georgiana Gligor <gb@tekkie.ro>
2016-09-03 10:43:09 +0300

    basic greeting bot

---
+++ app.js
@@ -0,0 +1,57 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+require('dotenv').load();
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url);
+});
+  
+// Create chat bot
+
+if (process.env.DEBUG) {
+  var connector = new builder.ConsoleConnector().listen();
+} else {
+  var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+  });
+}
+var bot = new builder.UniversalBot(connector);
+
+if (! process.env.DEBUG) {
+  server.post('/api/messages', connector.listen());
+}
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', [
+  function (session, args, next) {
+    if (! session.userData.name) {
+      session.beginDialog('/profile');
+    } else {
+      next();
+    }
+  },
+  function (session, results) {
+    session.send('Hello %s!', session.userData.name);
+  }
+]);
+
+bot.dialog('/profile', [
+  function (session) {
+    builder.Prompts.text(session, 'Hi! What is your name?');
+  },
+  function (session, results) {
+    session.userData.name = results.response;
+    session.endDialog();
+  }
+]);
